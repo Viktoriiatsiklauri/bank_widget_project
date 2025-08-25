ecce31cc6561b90e09a7fa9e48ba24225c858236
Victoria <viktoriiatsiklauri@mail.ru>
2025-08-25 21:26:28 +0300

    Добавлены конфигурационные файлы и HTML отчет покрытия

---
+++ bank_widget_project/src/processing/__init__.py
@@ -0,0 +1,31 @@
+# bank_widget_project/src/processing/__init__.py
+
+from typing import List, Dict
+
+
+def filter_by_state(data: List[Dict], state: str = "EXECUTED") -> List[Dict]:
+    """
+    Фильтрует список операций по значению state.
+    :param data: список словарей с операциями
+    :param state: значение поля 'state', по которому фильтруем (по умолчанию 'EXECUTED')
+    :return: отфильтрованный список операций
+    """
+    return [item for item in data if item.get("state") == state]
+
+
+from datetime import datetime
+from typing import List, Dict
+
+def sort_by_date(transactions: List[Dict], descending: bool = True) -> List[Dict]:
+    """
+    Сортирует список словарей по дате.
+
+    :param transactions: список словарей с ключом 'date'
+    :param descending: порядок сортировки (по убыванию, по умолчанию True)
+    :return: новый отсортированный список словарей
+    """
+    return sorted(
+        transactions,
+        key=lambda x: datetime.fromisoformat(x['date']),
+        reverse=descending
+    )
